e94d3eb11e1b1395ab69ecbb898f84aca2ed8986
sinofseven <em.s.00001@gmail.com>
2025-01-15 00:40:40 +0900

    fix implement for cloud_watch_alarm.py

--- aws_lambda_powertools/event_handler/async_execution/routes/cloud_watch_alarm.py
+++ aws_lambda_powertools/event_handler/async_execution/routes/cloud_watch_alarm.py
@@ -29,21 +29,49 @@ class CloudWatchAlarmRoute(BaseRoute):
         if not self.arn and not self.alarm_name and not self.alarm_name_prefix:
             raise ValueError("arn, alarm_name, or alarm_name_prefix must be not null")
 
+    def is_target_with_arn(self, arn: str | None) -> bool:
+        if not arn:
+            return False
+        elif self.arn:
+            return self.arn == arn
+        else:
+            return False
+
+    def is_target_with_alarm_name(self, alarm_name: str | None) -> bool:
+        if not alarm_name:
+            return False
+        elif self.alarm_name:
+            return self.alarm_name == alarm_name
+        elif self.alarm_name_prefix:
+            return alarm_name.find(self.alarm_name_prefix) == 0
+        else:
+            return False
+
     def match(self, event: dict[str, Any]) -> tuple[Callable, CloudWatchAlarmEvent] | None:
         if not isinstance(event, dict):
             return None
-        elif self.arn:
-            if event.get("alarmArn") == self.arn:
-                return self.func, CloudWatchAlarmEvent(event)
-            else:
-                return None
 
-        alarm_name = event.get("alarmData", {}).get("alarmName", "")
-        if self.alarm_name:
-            if alarm_name == self.alarm_name:
-                return self.func, CloudWatchAlarmEvent(event)
-        elif self.alarm_name_prefix:
-            if alarm_name.find(self.alarm_name_prefix) == 0:
-                return self.func, CloudWatchAlarmEvent(event)
+        arn: str | None = event.get("alarmArn")
+        alarm_name: str | None = event.get("alarmData", {}).get("alarmName")
 
-        return None
+        if not arn and not alarm_name:
+            return None
+
+        flag_arn = self.is_target_with_arn(arn=arn)
+        flag_alarm_name = self.is_target_with_alarm_name(alarm_name=alarm_name)
+
+        text = ", ".join(
+            ["arn: x" if arn is None else "arn: o", "alarm_name: x" if alarm_name is None else "alarm_name: o"],
+        )
+
+        mapping = {
+            "arn: o, alarm_name: o": flag_arn and flag_alarm_name,
+            "arn: o, alarm_name: x": flag_arn,
+            "arn: x, alarm_name: o": flag_alarm_name,
+            "arn: x, alarm_name: x": False,
+        }
+
+        if mapping[text]:
+            return self.func, CloudWatchAlarmEvent(event)
+        else:
+            return None
